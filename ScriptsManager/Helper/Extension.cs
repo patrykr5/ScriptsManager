c3d2dc36ae00a2e6355025f4ef787c2d2ffddb00
Patryk R <rajca.patryk@gmail.com>
2020-07-15 16:59:33 +0200

    creating menu actions

---
+++ ScriptsManager/Helper/Extension.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace ScriptsManager.Helper
+{
+    public static class Extension
+    {
+        /// <summary>
+        /// If value equal true then Visibility.Visible, if not then Visibility.Collapsed.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>Visibility</returns>
+        public static Visibility ToVisible(this bool value)
+        {
+            return value ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
--- ScriptsManager/ViewModel/HomeViewModel.cs
+++ ScriptsManager/ViewModel/HomeViewModel.cs
@@ -1,22 +1,76 @@
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using ScriptsManager.Helper;
 using System;
+using System.Windows;
 using System.Windows.Input;
 
 namespace ScriptsManager.ViewModel
 {
     public class HomeViewModel : ViewModelBase
     {
+        private Visibility _buttonOpenMenuVisibility;
+        private Visibility _buttonCloseMenuVisibility;
+
         public ICommand CloseCommand { get; }
+        public ICommand ButtonOpenMenuCommand { get; set; }
+        public ICommand ButtonCloseMenuCommand { get; set; }
 
         public HomeViewModel()
         {
             CloseCommand = new RelayCommand(CloseApplication);
+            ButtonOpenMenuCommand = new RelayCommand(ButtonOpenMenu);
+            ButtonCloseMenuCommand = new RelayCommand(ButtonCloseMenu);
+            ButtonCloseMenuVisibility = Visibility.Collapsed;
+        }
+
+        private void ButtonCloseMenu()
+        {
+            SetVisibilityForButtonOpenCloseMenu(whetherButtonOpenMenuIsClicked: false);
+        }
+
+        private void ButtonOpenMenu()
+        {
+            SetVisibilityForButtonOpenCloseMenu(whetherButtonOpenMenuIsClicked: true);
         }
 
         private void CloseApplication()
         {
             Environment.Exit(0);
+        }
+
+        private void SetVisibilityForButtonOpenCloseMenu(bool whetherButtonOpenMenuIsClicked)
+        {
+            ButtonOpenMenuVisibility = (!whetherButtonOpenMenuIsClicked).ToVisible();
+            ButtonCloseMenuVisibility = whetherButtonOpenMenuIsClicked.ToVisible();
+        }
+
+        public Visibility ButtonOpenMenuVisibility
+        {
+            get => _buttonOpenMenuVisibility;
+            set
+            {
+                if (_buttonOpenMenuVisibility == value)
+                {
+                    return;
+                }
+                _buttonOpenMenuVisibility = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public Visibility ButtonCloseMenuVisibility
+        {
+            get => _buttonCloseMenuVisibility;
+            set
+            {
+                if (_buttonCloseMenuVisibility == value)
+                {
+                    return;
+                }
+                _buttonCloseMenuVisibility = value;
+                RaisePropertyChanged();
+            }
         }
     }
 }
