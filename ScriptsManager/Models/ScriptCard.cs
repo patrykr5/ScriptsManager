142e91c1753e6c2ee2bf1dc397e14c004c435ee1
patrykr5 <rajca.patryk@gmail.com>
2020-08-22 18:04:22 +0200

    added script card structure and deleting cards

---
+++ ScriptsManager/Models/ScriptCard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ScriptsManager.Models
+{
+    public class ScriptCard
+    {
+        public int Id { get; set; }
+        public string ScriptName { get; set; }
+        public string FilePath { get; set; }
+        public DateTime LastRunDate { get; set; }
+        public ICommand EditCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+    }
+}
--- ScriptsManager/ViewModel/ManageViewModel.cs
+++ ScriptsManager/ViewModel/ManageViewModel.cs
@@ -1,17 +1,52 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using ScriptsManager.Models;
 using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ScriptsManager.ViewModel
 {
     public class ManageViewModel : ViewModelBase
     {
+        public ICommand AddCommand { get; private set; }
+        public ObservableCollection<ScriptCard> ScriptCardCollection { get; private set; } = new ObservableCollection<ScriptCard>();
+
         public ManageViewModel()
         {
+            AddCommand = new RelayCommand(Add);
+        }
 
+        private void Add()
+        {
+            const int numberOfScriptCardToAdd = 1;
+
+            ScriptCardCollection.Add(new ScriptCard()
+            {
+                Id = ScriptCardCollection.Count + numberOfScriptCardToAdd,
+                ScriptName = "Script name",
+                FilePath = "FileTestPath",
+                LastRunDate = DateTime.Now,
+                EditCommand = new RelayCommand<int>(rc => Edit(rc)),
+                DeleteCommand = new RelayCommand<int>(rc => Delete(rc))
+            });
+        }
+
+        private void Delete(int id)
+        {
+            ScriptCard scriptCardToDelete = GetScriptCardById(id);
+            ScriptCardCollection.Remove(scriptCardToDelete);
+        }
+
+        private void Edit(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        private ScriptCard GetScriptCardById(int id)
+        {
+            return ScriptCardCollection.First(x => x.Id == id);
         }
     }
 }
